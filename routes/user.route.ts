77b1c33289748fa1ecf3ccaafe16789236e8f8e5
Robert Nguyen <robert.nguyen279@gmail.com>
2022-03-29 16:13:02 +0700

    Fix some configurations & add first route

--- routes/user.route.ts
+++ routes/user.route.ts
@@ -3,8 +3,6 @@ import express, { Request, Response } from 'express'
 const router = express.Router()
 
 router.get('/', (req: Request, res: Response) => {
-  console.log('hihi')
-
   res.send({
     message: 'Welcome from user route'
   })
